796544a46bcad62b4192ebd248deb13225dfc0c6
I527874 <jasmita.chandran@sap.com>
2024-05-11 15:28:43 +0530

    -@GetMapping

--- src/main/java/com/example/helloworld/controller/BookController.java
+++ src/main/java/com/example/helloworld/controller/BookController.java
@@ -1,12 +1,11 @@
 package com.example.helloworld.controller;
 
 import com.example.helloworld.beans.Book;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 
@@ -33,31 +32,42 @@ import java.awt.*;
 //    }
 //}
 
+//@RestController
+////base url
+//@RequestMapping("/api")
+//public class BookController {
+//
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        return "Hello";
+//    }
+//
+//    //Give multiple URL (it would work same for /api/book ,/api/java) --> both will open same page
+//    //method = RequestMethod.GET (default http method --> GET(we can also use put,delete,post)
+//    @RequestMapping(value={"/book","/java"},
+//            method = RequestMethod.GET,
+//            //tells the rest api that produce the output in json/xml format
+//            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+//            //tells the rest api to consume the data in json/sml format
+//            consumes ={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+//    )
+//    public Book getBook(){
+//        Book book = new Book(1,"Java","Learn core java");
+//        return book;
+//
+//
+//    }
+//}
+
 @RestController
-//base url
-@RequestMapping("/api")
-public class BookController {
+public class BookController{
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello";
-    }
-
-    //Give multiple URL (it would work same for /api/book ,/api/java) --> both will open same page
-    //method = RequestMethod.GET (default http method --> GET(we can also use put,delete,post)
-    @RequestMapping(value={"/book","/java"},
-            method = RequestMethod.GET,
-            //tells the rest api that produce the output in json/xml format
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            //tells the rest api to consume the data in json/sml format
-            consumes ={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
-    )
+    @GetMapping(value={"/book","/java"})
     public Book getBook(){
         Book book = new Book(1,"Java","Learn core java");
         return book;
-
-
     }
+
 }
 
 
