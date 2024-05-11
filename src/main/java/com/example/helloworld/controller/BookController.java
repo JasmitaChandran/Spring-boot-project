33f8a2b5d8157d65cdde9f27f521500c460b0409
I527874 <jasmita.chandran@sap.com>
2024-05-11 11:45:09 +0530

    -@RestController

--- src/main/java/com/example/helloworld/controller/BookController.java
+++ src/main/java/com/example/helloworld/controller/BookController.java
@@ -4,16 +4,29 @@ import com.example.helloworld.beans.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//public class BookController {
+//
+//    @RequestMapping("/book")
+//    @ResponseBody
+//    public Book getBook(){
+//
+//        Book book = new Book(1,"Java","Learn core java");
+//        return book;
+//
+//    }
+//}
+
+//Rest Controller --> @Controller + @ResponseBody
+@RestController
 public class BookController {
-
     @RequestMapping("/book")
-    @ResponseBody
     public Book getBook(){
-
         Book book = new Book(1,"Java","Learn core java");
         return book;
-
     }
 }
+
+
